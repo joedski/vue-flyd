e1744cf07cf528cc821bab9a9d8047fad6c9d0b8
Joseph Sikorski <joedski@gmail.com>
2019-02-13 21:40:40 -0500

    Removed stream-prop special case
    It was incredibly fragile, and only worked if the stream prop never
    changed.  Use something like https://github.com/bertofer/flyd-mergeAll
    instead if you want to bind stream props.

--- es/StreamsManager.js
+++ es/StreamsManager.js
@@ -20,38 +20,22 @@ export default function StreamsManager(vm) {
         watchHandler = stream => next => stream(next)
       }
 
-      const sourceStream = (() => {
-        // If it's a stream, then subscribe to it directly, bypassing vue.
-        // We can check this because $createSource is called in $data, which is itself
-        // called in the data hook on the component, and props are available there.
-        if (typeof watchBinding === 'string' && vmHasProp(vm, watchBinding) && flyd.isStream(vm[watchBinding])) {
-          return Object.assign(flyd.map(
-            next => next,
-            vm[watchBinding]
-          ), {
-            // Noop to preserve interface.
-            $watch() {},
-          })
-        }
-        else {
-          return Object.assign(flyd.stream(), {
-            $watch() {
-              // Nothing to watch if we're not watching.
-              if (!watchBinding) return
+      const sourceStream = Object.assign(flyd.stream(), {
+        $watch() {
+          // Nothing to watch if we're not watching.
+          if (!watchBinding) return
 
-              vm.$watch(
-                // NOTE: When the watchBinding is a function, it's called
-                // with the vm as the context.
-                watchBinding,
-                watchHandler(sourceStream),
-                // NOTE: When passing `immediate: true`, a value will be
-                // immediately pushed into the stream!
-                watchOptions
-              )
-            },
-          })
-        }
-      })()
+          vm.$watch(
+            // NOTE: When the watchBinding is a function, it's called
+            // with the vm as the context.
+            watchBinding,
+            watchHandler(sourceStream),
+            // NOTE: When passing `immediate: true`, a value will be
+            // immediately pushed into the stream!
+            watchOptions
+          )
+        },
+      })
 
       this[name] = this.$sources[name] = sourceStream
 
