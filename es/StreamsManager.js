4f5431dd7ec57246e550060c76a7979a8ea577ea
Joseph Sikorski <joedski@gmail.com>
2019-02-14 16:54:13 -0500

    Maybe better manager and mixin?

--- es/StreamsManager.js
+++ es/StreamsManager.js
@@ -1,22 +1,9 @@
 import flyd from 'flyd'
 
-export default function StreamsManager(vm) {
-  const config = vm.$options.streams
-
-  const manager = {
-    $sources: {},
-    $sinks: {},
-    $controller: null,
-  }
-
-  manager.$controller = StreamsController(vm, config, manager)
-
-  return manager
-}
-
 // TODO: Don't need to pass in manager, can just return controller + sources + sinks
 // and do stuff with them to the manager.  Or just drop the manager name altogether.
-function StreamsController(vm, config, manager) {
+export default function StreamsManager(vm) {
+  const config = vm.$options.streams
   const hasConfig = !!config && typeof config === 'object'
 
   if (hasConfig && !(
@@ -28,22 +15,24 @@ function StreamsController(vm, config, manager) {
 
   return {
     watches: [],
+    sources: null,
+    sinks: null,
 
-    initializeStreams() {
+    createStreams() {
       if (!hasConfig) return
 
       // TODO: Check return value
-      const sources = config.sources.call(vm, {
+      this.sources = config.sources.call(vm, {
         fromWatch: (binding, options) => this.createStreamFromWatch(binding, options),
       })
 
       // TODO: Check return value
-      const sinks = config.sinks.call(vm, sources)
+      this.sinks = config.sinks.call(vm, this.sources)
 
-      Object.assign(manager.$sources, sources)
-      // Alias sources to the manager for lazy access.
-      Object.assign(manager, sources)
-      Object.assign(manager.$sinks, sinks)
+      return {
+        sources: this.sources,
+        sinks: this.sinks,
+      }
     },
 
     createStreamFromWatch(binding, options) {
@@ -55,7 +44,7 @@ function StreamsController(vm, config, manager) {
     },
 
     data() {
-      return Object.entries(manager.$sinks).reduce(
+      return Object.entries(this.sinks).reduce(
         (acc, [key, stream]) => {
           // TODO: This?  Or undefined?  Hm.
           acc[key] = stream()
@@ -66,81 +55,16 @@ function StreamsController(vm, config, manager) {
     },
 
     watch() {
-      Object.entries(manager.$sinks).forEach(([key, stream]) => {
+      Object.entries(this.sinks).forEach(([key, stream]) => {
         stream.pipe(flyd.on(e => { vm[key] = e }))
       })
       this.watches.forEach(watchFn => watchFn())
     },
 
     end() {
-      Object.values(manager.$sources).forEach((stream) => {
+      Object.values(this.sources).forEach((stream) => {
         stream.end(true)
       })
     },
   }
 }
-
-// export default function StreamsManager(vm) {
-//   const config = vm.$options.streams
-//   return {
-//     $sources: {},
-//     $sinks: null,
-
-//     $createSource(
-//       name,
-//       watchBinding = name,
-//       watchOptions = { immediate: true }
-//     ) {
-//       const sourceStream = Object.assign(flyd.stream(), {
-//         $watch() {
-//           // Nothing to watch if we're not watching.
-//           if (!watchBinding) return
-
-//           vm.$watch(
-//             // NOTE: When the watchBinding is a function, it's called
-//             // with the vm as the context.
-//             watchBinding,
-//             next => sourceStream(next),
-//             // NOTE: When passing `immediate: true`, a value will be
-//             // immediately pushed into the stream!
-//             watchOptions
-//           )
-//         },
-//       })
-
-//       this[name] = this.$sources[name] = sourceStream
-
-//       return sourceStream
-//     },
-
-//     $data() {
-//       this.$sinks = config
-//         ? config.call(
-//           vm,
-//           (name, watchBinding, watchOptions) =>
-//             this.$createSource(name, watchBinding, watchOptions)
-//         )
-//         : {}
-
-//       return Object.keys(this.$sinks).reduce(
-//         (acc, key) => {
-//           acc[key] = this.$sinks[key]()
-//           return acc
-//         },
-//         {}
-//       )
-//     },
-
-//     $watch() {
-//       Object.keys(this.$sources).forEach(key => this.$sources[key].$watch())
-//       Object.keys(this.$sinks).forEach(key => flyd.on(
-//         v => { vm[key] = v },
-//         this.$sinks[key]
-//       ))
-//     },
-
-//     $end() {
-//       Object.keys(this.$sources).forEach(key => this.$sources[key].end(true))
-//     }
-//   }
-// }
--- es/StreamsMixin.js
+++ es/StreamsMixin.js
@@ -2,20 +2,28 @@ import StreamsManager from './StreamsManager'
 
 export default {
   beforeCreate() {
-    this.$streams = StreamsManager(this)
-    this.$streams.$controller.initializeStreams()
+    const $manager = StreamsManager(this)
+    const { sources, sinks } = $manager.createStreams()
+
+    this.$streams = {
+      // Spread sources into this for convenience' sake.
+      ...sources,
+      $sources: sources,
+      $sinks: sinks,
+      $manager,
+    }
   },
 
   // data() comes before created(), as per the vue lifecycle spec.
   data() {
-    return this.$streams.$controller.data()
+    return this.$streams.$manager.data()
   },
 
   created() {
-    this.$streams.$controller.watch()
+    this.$streams.$manager.watch()
   },
 
   beforeDestroy() {
-    this.$streams.$controller.end()
+    this.$streams.$manager.end()
   },
 }
